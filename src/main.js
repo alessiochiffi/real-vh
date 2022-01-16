714e8e81f75bbb5069ebe9e035a7594f0ad0f77b
Alessioch <alessiochf@gmail.com>
2022-01-16 15:39:02 +0000

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import realVh from './plugin/real-vh';
 
 Vue.config.productionTip = false;
+
+Vue.use(realVh);
 
 new Vue({
   render: (h) => h(App),
---
+++ src/plugin/real-vh.js
@@ -0,0 +1,58 @@
+import throttle from '../util';
+
+export default {
+  install(Vue, options) {
+    let vh = window.innerHeight * 0.01;
+    let isRealVhSet = false;
+    const cssVarName = options && options.varName ? options.varName : 'vh';
+
+    Vue.mixin({
+      created() {
+        if (isRealVhSet) {
+          return;
+        }
+
+        this.setRealVh();
+      },
+      mounted() {
+        window.addEventListener('resize', () => {
+          throttle(this.updateRealVh(), 100);
+        });
+      },
+      methods: {
+        addStyleTag(styleString) {
+          const styleTag = document.createElement('style');
+          styleTag.classList.add('real-vh');
+          styleTag.textContent = styleString;
+          document.head.append(styleTag);
+          isRealVhSet = true;
+        },
+        updateStyle(styleString) {
+          const styleTag = document.querySelector('.real-vh');
+
+          if (!styleTag) {
+            return;
+          }
+
+          styleTag.textContent = styleString;
+        },
+        setRealVh() {
+          this.addStyleTag(this.getCss());
+        },
+        updateRealVh() {
+          this.updateStyle(this.getCss());
+        },
+        getCss() {
+          vh = window.innerHeight * 0.01;
+
+          return `:root {
+            --${cssVarName}: ${vh}px;
+            --${cssVarName}50: calc(var(--${cssVarName}, 1vh) * 50);
+            --${cssVarName}100: calc(var(--${cssVarName}, 1vh) * 100);
+          }
+        `;
+        },
+      },
+    });
+  },
+};
